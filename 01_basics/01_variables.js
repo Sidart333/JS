90d558cdbb816c0fafc074cb9e0b3e2ae39e238d
Siddharth <135261769+Sidart333@users.noreply.github.com>
2023-12-05 18:49:35 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 2345678
+let emailId = "siddharth@gmail.com"
+var accountPassword = 987654321
+accountCity = "Jodhpur"
+let accountState
+
+console.table([accountId, emailId, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// let name="Siddharth";
+// let age= 23;
+// let isLoggedIn = true;
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined 
+// symbol
+ 
+console.log(typeof null);
+
+console.log(typeof 34);
+
+console.log(typeof undefined);
