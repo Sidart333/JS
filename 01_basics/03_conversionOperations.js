c8fa1a9711170eaac31386049c95e565dd3c68d7
Siddharth <135261769+Sidart333@users.noreply.github.com>
2023-12-08 18:47:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+
+// let num = null
+
+// let x = undefined
+
+// console.log(typeof score);
+// console.log(typeof(scope));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(num);
+// console.log(x);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ 
+console.log(booleanIsLoggedIn);
+
+// "" => false
+// "siddharth" => true
+
+let y = 44
+let strnum = String(y)
+console.log(typeof strnum);
