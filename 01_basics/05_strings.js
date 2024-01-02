bab2176b6de83929cc1fbd4e5c2da2888d55677b
Siddharth <135261769+Sidart333@users.noreply.github.com>
2024-01-02 17:59:50 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+let name = 'siddharth'
+let repoCount = 5;
+
+//the benefit of using string interpolation is we can also add some methods
+
+console.log(`hey my name is ${name} and my repo count is ${repoCount}`);
+
+const myName = "sidartistic";
+
+console.log(myName);
+
+console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(4));
+// console.log(myName.indexOf('i'));
+
+const str = myName.substring(0,6);
+console.log(str);
+
+const str2 = myName.slice(-8,5);
+console.log(str2);
+
+const str3 = "   sidart    ";
+console.log(str3);
+console.log(str3.trim());
