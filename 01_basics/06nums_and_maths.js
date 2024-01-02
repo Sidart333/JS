e56a63384cb26017b82ee71ac0cb666694e6f10a
Siddharth <135261769+Sidart333@users.noreply.github.com>
2024-01-02 18:18:04 +0000

    number and maths methods in js

---
+++ 01_basics/06nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 599;
+
+const num = new Number(300);
+// console.log(score);
+// console.log(num);
+// console.log(score.toString());
+// console.log(typeof(score));
+
+// console.log(score.toFixed(2));
+
+const otherNumber = 43.33234;
+
+// console.log(otherNumber.toPrecision());
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-42));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,3,2));
+// console.log(Math.max(5,3,9,7));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
